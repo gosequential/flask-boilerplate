2331c748110abdc033d02e4f8ab7bddeb3514cba
enoch <greg@deaconjames.com>
2015-04-20 08:49:13 -0400

    removed cruft from views.py

--- package/app/main/views.py
+++ package/app/main/views.py
@@ -1,9 +1,6 @@
 from datetime import datetime
 from flask import render_template
 from . import main
-#from .forms import NameForm
-#from .. import db
-#from ..models import User
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
